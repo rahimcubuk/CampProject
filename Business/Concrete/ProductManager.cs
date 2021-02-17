b2f3a9e79a07c5402a0d7cf358798e727adead06
rahim çubuk <rahimcubuk@gmail.com>
2021-02-18 00:18:40 +0300

    18.02.2021 ders sonu guncellemesi

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,16 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
-using Core.CrossCuttingConcerns.Validation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
-using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
-using FluentValidation;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Business.Concrete
 {
@@ -26,11 +23,14 @@ namespace Business.Concrete
         #endregion
 
         #region Methods
-
+        /*
+         * Interceptors --> projelerde validasyon hatasi alidiginda oncesinde sonrasinda calisacak kodlarin yonetimi
+         * Attribute yontemi
+         * 
+         * */
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            ValidationTool.Validate(new ProductValidator(), product);
-
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
@@ -42,22 +42,22 @@ namespace Business.Concrete
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
 
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return  new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==id));
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
         }
 
         public IDataResult<Product> GetById(int productId)
         {
-            return new SuccessDataResult<Product>(_productDal.Get(p=>p.ProductId == productId));
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
 
         public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
         {
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.UnitPrice>=min && p.UnitPrice<=max));
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,11 +1,11 @@
 ﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Business.DependencyResolvers.Autofac
 {
@@ -32,21 +32,22 @@ namespace Business.DependencyResolvers.Autofac
              */
             // kodunu ekledik. 
             #region Product IoC Container Code Block
-            builder.RegisterType<ProductManager>()
-                   .As<IProductService>()
-                   .SingleInstance();
-            builder.RegisterType<EfProductDal>()
-                   .As<IProductDal>()
-                   .SingleInstance();
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
             #endregion
 
             #region Category IoC Container Code Block
-            builder.RegisterType<CategoryManager>()
-                   .As<ICategoryService>()
-                   .SingleInstance();
-            builder.RegisterType<EfCategoryDal>()
-                   .As<ICategoryDal>()
-                   .SingleInstance();
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+            #endregion
+
+            #region Aspect
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+               .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+               {
+                   Selector = new AspectInterceptorSelector()
+               }).SingleInstance();
             #endregion
         }
     }
